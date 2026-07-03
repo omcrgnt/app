9493a8fa7759c31efc09f4977dddff3225a5b271
mcrgnt <microgiant@yandex.ru>
2026-07-04 01:33:24 +0300

    fix fill generic types panic (#4)

--- catalog.go
+++ catalog.go
@@ -1,6 +1,7 @@
 package app
 
-// Configurable is a catalog wire type that registers a config spec for env + materialize.
+// Configurable is a catalog resource slot (*Resource or value) that registers a spec for env + materialize.
+// Nil *Resource is valid: fill calls BuildConfig on a zero instance of the field type.
 type Configurable interface {
 	BuildConfig() (Materializer, error)
 }
@@ -10,7 +11,8 @@ type Materializer interface {
 	Build() (any, error)
 }
 
-// ResourceFactory is a catalog wire type whose resource is created at fill time.
+// ResourceFactory is a catalog resource slot whose runtime instance is created at fill time.
+// Nil *Resource is valid: fill calls NewResource on a zero instance of the field type.
 type ResourceFactory interface {
 	NewResource() (any, error)
 }
